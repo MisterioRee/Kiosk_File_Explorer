e792bb1679266c30bbe345346d68bc0027bf74b7
Muhammad Rehan <mrmuhammadrehan65@gmail.com>
2020-08-07 23:29:19 +0400

    App Content

---
+++ Directory/Data/DirectoryItemType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk_File_Explorer.Directory.Data
+{
+    /// <summary>
+    /// The type of a directory item
+    /// </summary>
+    public enum DirectoryItemType
+    {
+        /// <summary>
+        /// A logical drive
+        /// </summary>
+        Drive,
+        /// <summary>
+        /// A phyiscal file
+        /// </summary>
+        File,
+        /// <summary>
+        /// A folder
+        /// </summary>
+        Folder
+    }
+}
---
+++ Directory/ViewModels/DirectoryStructureViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Kiosk_File_Explorer.Directory.Data;
+using Kiosk_File_Explorer.Directory.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk_File_Explorer.Directory.ViewModels
+{
+    /// <summary>
+    /// The view model for the applications main Directory view
+    /// </summary>
+    public class DirectoryStructureViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// A list of all directories on the machine
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Items { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DirectoryStructureViewModel()
+        {
+            // Get the logical drives
+            var children = DirectoryStructure.GetLogicalDrives();
+
+            // Create the view models from the data
+            this.Items = new ObservableCollection<DirectoryItemViewModel>(
+                children.Select(drive => new DirectoryItemViewModel(drive.FullPath, DirectoryItemType.Drive)));
+        }
+
+        #endregion
+    }
+}
